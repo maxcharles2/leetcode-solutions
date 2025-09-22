df77365b1a126ebad8f134be0f42cd538a10b679
Max Charles II <mcharles2016@yahoo.com>
2025-09-22 18:30:03 -0400

    valid palindrome solution

---
+++ two-pointers/valid-palindrome.js
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/valid-palindrome/description/
+//check on neetcode
+
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+
+    //taking in a string as an input
+    //returning boolean as a result
+    //"tab a cat" => false
+    //as long as it can reverse and be the same true
+    //if not then false
+    //two pointers to the center and fail if doesn't match
+    isPalindrome(s) {
+        let cleaned = s.replace(/[^a-z0-9]/gi, "").toLowerCase();
+
+        let i = 0
+        let j = cleaned.length - 1;
+
+        while (i < j){
+
+            if(cleaned[i] !== cleaned[j]){
+                return false;
+            }
+
+            i++
+            j--
+        }
+
+        return true;
+    }
+}
