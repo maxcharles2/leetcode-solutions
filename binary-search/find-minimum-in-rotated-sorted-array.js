e5103bf71a8c11b103a364286e21e1190b00654c
Max Charles II <mcharles2016@yahoo.com>
2025-10-26 22:55:15 -0400

    find minimum in rotated sorted array solution

---
+++ binary-search/find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMin(nums) {
+        let left = 0;
+        let right = nums.length - 1;
+        let result = nums[0];
+
+        while (left <= right) {
+            if (nums[left] < nums[right]) {
+            result = Math.min(result, nums[left]);
+            break;
+            }
+
+            const mid = Math.floor((left + right) / 2);
+            result = Math.min(result, nums[mid]);
+
+            if (nums[mid] >= nums[left]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
